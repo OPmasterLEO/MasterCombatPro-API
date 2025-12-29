c86dcb5714d464371d7446494ea3c9bc2649ed2d
OPmasterLEO <98689894+OPmasterLEO@users.noreply.github.com>
2025-12-29 23:08:37 +0100

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,41 @@
 plugins {
     `java-library`
+    `maven-publish`
+}
+
+group = "com.github.OPmasterLEO"
+version = "1.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("org.jetbrains:annotations:26.0.2-1")
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     
-    implementation("com.github.OPmasterLEO:MasterCombatPro-API:1.0.0")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+
+    implementation("ca.spottedleaf:concurrentutil:0.0.7")
 }
 
 java {
     withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(21)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
