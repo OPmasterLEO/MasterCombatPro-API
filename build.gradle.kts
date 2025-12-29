dd556a74482a83d13b8ae3ff9afec32e9bd765bd
OPmasterLEO <98689894+OPmasterLEO@users.noreply.github.com>
2025-12-29 23:04:44 +0100

    Update dependency and add JitPack configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     compileOnly("org.jetbrains:annotations:26.0.2-1")
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     
-    implementation("net.opmasterleo.Combat:master-SNAPSHOT")
+    implementation("com.github.OPmasterLEO:MasterCombatPro-API:1.1.0")
 }
 
 java {
